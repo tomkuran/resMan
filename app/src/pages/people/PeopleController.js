d40f69a093d291374935f0af0b053933c4420ed7
Tomasz Kuran <tomkuran@gmail.com>
2016-05-13 17:11:02 +0200

    Dodałem funkcje do controllera
    Mogę bazując na tym rozwijac CRUD

--- app/src/pages/people/PeopleController.js
+++ app/src/pages/people/PeopleController.js
@@ -3,8 +3,21 @@
   angular
        .module('peopleModule')
        .controller('PeopleController', ['$scope', 'People', function($scope, People) {
+         var self = this;
+         self.showToast = showToast;
+         self.getPerson = getPerson;
            $scope.people = People.query();
-           $scope.orderProp = 'age';
+           $scope.orderProp = 'firstName';
+
+
+           function showToast($event){
+             console.log($event);
+           }
+
+
+           function getPerson(personId) {
+             console.log(People.get({id:personId}));
+           }
 }]);
 
 })();
